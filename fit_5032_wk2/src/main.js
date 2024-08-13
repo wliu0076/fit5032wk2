ee429752c11fb498c2f41fb258043cda24db6170
wliu0076 <wliu0076@student.monash.edu>
2024-08-14 02:40:53 +1000

    activity 4+ finished

--- fit_5032_wk2/src/main.js
+++ fit_5032_wk2/src/main.js
@@ -1,9 +1,18 @@
-
-import { createApp } from "vue"
-import App from "./App.vue"
+import { createApp } from "vue";
+import App from "./App.vue";
 
 // import bootstrap css
-import 'bootstrap/dist/css/bootstrap.min.css'
-import './style.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+// import './style.css';
+
+import PrimeVue from 'primevue/config';
+import Aura from '@primevue/themes/aura';
+
+const app = createApp(App);
+app.use(PrimeVue, {
+    theme: {
+        preset: Aura
+    }
+});
 
 createApp(App).mount("#app")
